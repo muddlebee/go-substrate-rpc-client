6e456bb75b7ac90546373d7065f7d3f81dbef7c9
Anwesh <anweshknayak@gmail.com>
2023-09-13 03:17:21 +0530

    init: examples

---
+++ examples/listenNewBlocks.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	gsrpc "github.com/centrifuge/go-substrate-rpc-client"
+	"github.com/centrifuge/go-substrate-rpc-client/v4/config"
+	"runtime/debug"
+)
+
+func main() {
+	// This example shows how to instantiate a Substrate API and use it to connect to a node and retrieve balance
+	// updates
+	//
+	// NOTE: The example runs until you stop it with CTRL+C
+	api, err := gsrpc.NewSubstrateAPI(config.Default().RPCURL)
+	if err != nil {
+		fmt.Println("Error occurred:", err)
+		debug.PrintStack()
+		return
+	}
+
+	res, err := api.RPC.Author.PendingExtrinsics()
+	fmt.Printf("Pending extrinsics: %v\n", res)
+	meta, err := api.RPC.State.GetMetadataLatest()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("meta: %v\n", meta)
+
+}
